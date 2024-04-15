55132193af87c1d6baf3d52e79dc3651e2cb25d0
FernandoGaribay <garibaycejaf@gmail.com>
2024-04-15 00:55:19 -0600

    + Division por hilos

--- src/main/Game.java
+++ src/main/Game.java
@@ -24,7 +24,7 @@ public class Game extends Canvas implements Runnable {
     private static final int MILLIS_POR_SEGUNDO = 1000;
     private static final int NANOS_POR_SEGUNDO = 1000000000;
     private static final double NUM_TICKS = 60.0;
-    private static final double NUM_FRAMES = 75.0;
+    private static final double NUM_FRAMES = 60.0;
     private static final String NOMBRE = "Super Mario Bros";
 
     private static final int SCREEN_OFFSET = 32 * 1; // 1 bloque de offset
@@ -36,9 +36,13 @@ public class Game extends Canvas implements Runnable {
 
     // VARIABLES
     private boolean running;
+    private int frames = 0;
+    private int updates = 0;
 
     // GAME COMPONENTS
-    private Thread hilo;
+    private Thread hiloPrincipal;
+    private Thread updateThread;
+    private Thread renderThread;
     private Handler handler;
     private Ventana ventana;
     private Camara camara;
@@ -50,12 +54,12 @@ public class Game extends Canvas implements Runnable {
 
     // <editor-fold defaultstate="collapsed" desc="Inizializar Elementos">  
     private void inicializarElementos() {
-        this.addKeyListener(new KeyInput(handler));
-
         handler = new Handler();
         ventana = new Ventana(VENTANA_WIDTH, VENTANA_HEIGHT, NOMBRE);
         camara = new Camara(0, SCREEN_OFFSET);
         loadScreen = new LoadScreen(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        this.addKeyListener(new KeyInput(handler));
 
         ventana.setCanvas(loadScreen);
         handler.addObj(new Background(0, 0, VENTANA_WIDTH, VENTANA_HEIGHT, SCREEN_OFFSET, camara));
@@ -105,42 +109,52 @@ public class Game extends Canvas implements Runnable {
     // </editor-fold>  
 
     private synchronized void gameLoopInicio() {
-        this.hilo = new Thread(this);
-        this.hilo.start();
+        this.hiloPrincipal = new Thread(this);
+        this.updateThread = new Thread(this::runUpdate);
+        this.renderThread = new Thread(this::runRender);
+
         this.running = true;
+        hiloPrincipal.start();
     }
 
     private synchronized void gameLoopParar() {
         try {
-            this.hilo.join();
             this.running = false;
+            this.updateThread.join();
+            this.renderThread.join();
         } catch (InterruptedException ex) {
             Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
         }
-        this.running = false;
     }
 
     @Override
     public void run() {
-        double ticksDeseados = NUM_TICKS; // representa cuantas veces se actualiza el juego por segundo
-        double framesDeseados = NUM_FRAMES; // representa cuantas veces se actualiza el juego por segundo
+        updateThread.start();
+        renderThread.start();
 
-        double nsTicks = NANOS_POR_SEGUNDO / ticksDeseados; // cuantos nanosegundos deben pasar entre cada actualizacion para alcanzar amountOfTicks por segundo
-        double nsFrames = NANOS_POR_SEGUNDO / framesDeseados; // cuantos nanosegundos deben pasar entre cada frames para alcanzar amountOfTicks por segundo
+        while (running) {
+            try {
+                Thread.sleep(1000);
+
+                System.out.println("FPS: " + frames + " TPS: " + updates);
+                updates = 0;
+                frames = 0;
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void runUpdate() {
+        double nsTicks = NANOS_POR_SEGUNDO / NUM_TICKS; // cuantos nanosegundos deben pasar entre cada actualizacion para alcanzar amountOfTicks por segundo
 
         double deltaTicks = 0; // controla cuando se debe ejecutar el metodo tick()
-        double deltaFrames = 0; // controla cuando se debe ejecutar el metodo tick()
-
-        int frames = 0; // estadisticas de FPS (cuadros por segundo) y TPS (actualizaciones por segundo).
-        int updates = 0; // estadisticas de FPS (cuadros por segundo) y TPS (actualizaciones por segundo).
 
         long lastTime = System.nanoTime(); // calcular cuanto tiempo ha pasado desde la ultima actualizacion del juego
-        long timer = System.currentTimeMillis(); // controla cuando imprimir FPS y TPS y restablecer el contador de updates) y frames
 
         while (running) {
             long now = System.nanoTime();
             deltaTicks += (now - lastTime) / nsTicks;
-            deltaFrames += (now - lastTime) / nsFrames;
             lastTime = now;
 
             while (deltaTicks >= 1) {
@@ -148,19 +162,27 @@ public class Game extends Canvas implements Runnable {
                 updates++;
                 deltaTicks--;
             }
+        }
+    }
+
+    private void runRender() {
+        double nsFrames = NANOS_POR_SEGUNDO / NUM_FRAMES; // cuantos nanosegundos deben pasar entre cada frames para alcanzar amountOfTicks por segundo
+
+        double deltaFrames = 0; // controla cuando se debe ejecutar el metodo render()
+
+        long lastTime = System.nanoTime(); // calcular cuanto tiempo ha pasado desde la ultima renderización del juego
+
+        while (running) {
+            long now = System.nanoTime();
+            deltaFrames += (now - lastTime) / nsFrames;
+            lastTime = now;
+
             while (deltaFrames >= 1) {
                 render();
                 frames++;
                 deltaFrames--;
             }
-            if (System.currentTimeMillis() - timer > MILLIS_POR_SEGUNDO) {
-                timer += MILLIS_POR_SEGUNDO;
-                System.out.println("FPS: " + frames + " TPS: " + updates);
-                updates = 0;
-                frames = 0;
-            }
         }
-        gameLoopParar();
     }
 
     private void tick() {
